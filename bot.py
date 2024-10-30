889efecf2e021dcce8569f113dd628281ddbf5ef
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 12:23:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,22 @@
 # bot.py
 
+import logging
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from plugins.start import start
 from plugins.callback import handle_callback
-from plugins.updater import main as updater_main  # Import updater
 from config import BOT_TOKEN
 
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 def main():
-    # Membuat instance Updater
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Menambahkan handler untuk perintah /start
     dp.add_handler(CommandHandler("start", start))
-
-    # Menambahkan handler untuk callback query
     dp.add_handler(CallbackQueryHandler(handle_callback))
 
-    # Menambahkan handler untuk perintah /update
-    updater_main(dp)  # Memanggil fungsi updater_main untuk menambahkan handler update
-
-    # Memulai polling
+    logging.info("Starting bot...")
     updater.start_polling()
-    # Menunggu sampai bot dihentikan
     updater.idle()
-
-if __name__ == '__main__':
-    main()
