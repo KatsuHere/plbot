05652bf8513efec6af7e41e8d56f83a89f18e167
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 10:55:21 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+BOT_TOKEN = "7804488689:AAEupsmSGDp1FD3NvYqGZPP-NrEzkBHBd4w"
+API_ID = "28316053"
+API_HASH = "8be6a8a307b7d27aa31b13ce5f2d66ac"
+OWNER_ID = 7494727691
+OWNER_USERNAME = "@KatsuHere"
+LOGO_URL = "https://nyxiannetwork.web.id/uploader/img/nyx37c15ce204d03bb05838164e1870d8c5.jpg"  # URL gambar logo
