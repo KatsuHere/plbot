9914806f18489829d30f1dbb1a50085006057f4a
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 11:20:38 +0700

    Update data.py

--- plugins/data.py
+++ plugins/data.py
@@ -4,16 +4,16 @@ from config import OWNER_USERNAME  # Pastikan OWNER_USERNAME didefinisikan di co
 
 class DataProduk:
     pricelist_vps = [
-        "List Harga VPS NyxianNetwork:\n",
+        "*List Harga VPS NyxianNetwork:*\n",
         "---",
-        "Harga VPS dan Benefit\n",
+        "*Harga VPS dan Benefit*\n",
         "- VPS 2GB RAM, 1 Core — Rp20.000/bulan",
         "- VPS 2GB RAM, 2 Core — Rp32.000/bulan",
         "- VPS 4GB RAM, 2 Core — Rp45.000/bulan",
         "- VPS 8GB RAM, 2 Core — Rp50.000/bulan",
         "- VPS 8GB RAM, 4 Core — Rp55.000/bulan",
         "- VPS 16GB RAM, 4 Core — Rp70.000/bulan\n",
-        "Benefit:",
+        "*Benefit:*",
         "- Gratis Instal Panel Pterodactyl",
         "- Gratis Instal Wings/Node",
         "- Gratis Instal Tema Panel (khusus RAM 8GB - 16GB)",
