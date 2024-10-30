377741a5aeec6048dca0f37e536c544e2660f25a
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 11:50:03 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,15 +15,18 @@ def start(update, context: CallbackContext):
     # Mengirimkan pesan awal dengan huruf "S"
     message = context.bot.send_message(chat_id=chat_id, text="S")
 
-    # Memperbarui pesan secara bertahap untuk efek animasi
+    # Memperbarui pesan secara bertahap untuk efek animasi lebih cepat
     for i in range(1, len(typing_text) + 1):
-        time.sleep(0.5)  # jeda antar karakter untuk efek mengetik
+        time.sleep(0.1)  # jeda antar karakter untuk efek mengetik yang lebih cepat
         try:
             context.bot.edit_message_text(chat_id=chat_id, message_id=message.message_id, text=typing_text[:i])
         except Exception as e:
             print(f"Error editing message: {e}")  # Menangani jika terjadi error saat edit
 
-    # Setelah animasi selesai, kirimkan gambar, caption, dan tombol
+    # Menghapus pesan animasi setelah selesai
+    context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+
+    # Menyiapkan keyboard dengan tombol
     keyboard = [
         [
             InlineKeyboardButton("Vps", callback_data="vps"),
@@ -39,10 +42,10 @@ def start(update, context: CallbackContext):
     caption_text = "*Welcome to NyxianNetwork!*" \
                    "\nSilakan pilih layanan yang ingin Anda lihat harganya."
 
-    # Mengirimkan pesan gambar, caption, dan tombol
+    # Mengirimkan pesan baru dengan gambar, caption, dan tombol
     context.bot.send_photo(
         chat_id=chat_id,
-        photo=LOGO_URL,  # Menggunakan LOGO_URL
+        photo=LOGO_URL,
         caption=caption_text,
         reply_markup=reply_markup,
         parse_mode=ParseMode.MARKDOWN_V2
