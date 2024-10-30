89cb2ee559ac3b361aefca8e166965aea2eba6a4
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 10:47:17 +0700

    Create data.py

---
+++ plugins/data.py
@@ -0,0 +1,20 @@
+# plugins/data.py
+
+class DataProduk:
+    pricelist_vps = [
+        "Paket VPS 1: Rp100.000/bulan",
+        "Paket VPS 2: Rp200.000/bulan",
+        "Paket VPS 3: Rp300.000/bulan"
+    ]
+
+    pricelist_userbot = [
+        "Paket Userbot 1: Rp50.000/bulan",
+        "Paket Userbot 2: Rp75.000/bulan",
+        "Paket Userbot 3: Rp100.000/bulan"
+    ]
+
+    pricelist_bot_fsub = [
+        "Paket Bot Fsub 1: Rp150.000/bulan",
+        "Paket Bot Fsub 2: Rp250.000/bulan",
+        "Paket Bot Fsub 3: Rp350.000/bulan"
+    ]
