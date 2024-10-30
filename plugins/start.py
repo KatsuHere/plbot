fdad72dd12bf8e3b687bde00acb06f90460408ef
KatsuHere <45770031+KatsuHere@users.noreply.github.com>
2024-10-30 11:52:23 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,7 @@
 import time
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import CallbackContext
-from config import LOGO_URL, OWNER_USERNAME  # Mengimpor LOGO_URL
+from config import LOGO_URL, OWNER_USERNAME
 from plugins.data import DataProduk
 
 def start(update, context: CallbackContext):
@@ -24,7 +24,10 @@ def start(update, context: CallbackContext):
             print(f"Error editing message: {e}")  # Menangani jika terjadi error saat edit
 
     # Menghapus pesan animasi setelah selesai
-    context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+    try:
+        context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+    except Exception as e:
+        print(f"Error deleting message: {e}")
 
     # Menyiapkan keyboard dengan tombol
     keyboard = [
@@ -42,7 +45,7 @@ def start(update, context: CallbackContext):
     caption_text = "*Welcome to NyxianNetwork!*" \
                    "\nSilakan pilih layanan yang ingin Anda lihat harganya."
 
-    # Mengirimkan pesan baru dengan gambar, caption, dan tombol
+    # Mengirimkan pesan baru dengan gambar, caption, dan tombol setelah animasi dihapus
     context.bot.send_photo(
         chat_id=chat_id,
         photo=LOGO_URL,
